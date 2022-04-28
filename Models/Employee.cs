e2211a709df54aa5d326e945467acdda3a074165
PallaviKatari <pallavikatari@gmail.com>
2022-04-28 17:26:24 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstCoreApp.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        //[Required(ErrorMessage = "Kindly fill your Name")]
+        //[StringLength(15, MinimumLength = 4, ErrorMessage = "Name does not satisfy the condition")]
+        //[RegularExpression(@"^([A-Za-z]+)$")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Kindly fill your Designation")]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Designation does not satisfy the condition")]
+        [RegularExpression(@"^([A-Za-z]+)$")]
+        public string? Designation { get; set; }
+    }
+}
